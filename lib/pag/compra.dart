36a53673d705e7ef0c8a020f2f2564f81500b866
Osmin9930 <osmin9930@gmail.com>
2021-11-21 20:13:15 -0600

    acti

---
+++ lib/pag/compra.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class compra extends StatelessWidget {
+  const compra({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("compra"),
+      ),
+      body: Column(),
+    );
+  }
+}
